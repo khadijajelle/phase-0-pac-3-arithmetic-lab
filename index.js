74e38589c0ed21e719eb36b86da8315583f1da9a
khadija <khadijajelle3@gmail.com>
2024-06-12 17:55:30 +0300

    complete arithmetic-lab

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(math) {
+    
+  }
+  function subtract (math) {
+    
+  }
+  function multiply(math) {
+    
+  }
+  function divide (math) {
+    
+  }
+  function add(a,b) {
+    return (a+b)
+  }
+  function subtract (a,b) {
+    return (a-b)
+    
+  }
+  function multiply(a,b) {
+    return (a*b)
+  }
+  function divide (a,b) {
+    return (a/b)
+  }
+  function  increment(n){
+    return n+1
+  }
+  function  decrement(n){
+    return n-1
+  }
+  function  makeInt(string){
+    return  parseInt(string, 10)
+  }
+  function  preserveDecimal(string){
+    return  parseFloat(string)
+  }
